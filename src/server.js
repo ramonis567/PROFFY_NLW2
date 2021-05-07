0fd1843ad394d49a5d89ee96b3b50b8e950cc4df
ramonis567 <ramonmatosferreira@gmail.com>
2021-05-07 17:50:04 -0300

    nunjucks data show

--- src/server.js
+++ src/server.js
@@ -1,8 +1,36 @@
+const proffys = [
+  {
+    name: "Ramon Matos",
+    avatar: "https://cdn.icon-icons.com/icons2/1879/PNG/512/iconfinder-7-avatar-2754582_120519.png",
+    subject: "Matemática", 
+    bio : "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste, autem dolorum accusantium ducimus architecto distinctio vero. Odio eaque unde, vero perspiciatis illum voluptatibus sunt quod excepturi! Illo natus sed magni!",
+    cost: "20",
+    whatsapp: "33988928922",
+    weekday: [2,3,4,5,6], 
+    time_from: [720, 720, 720, 720, 720], 
+    time_to: [810, 810, 810, 810, 810]
+  },
+  {
+    name: "Ramon Ferreira",
+    avatar: "https://cdn.icon-icons.com/icons2/1879/PNG/512/iconfinder-7-avatar-2754582_120519.png",
+    subject: "Química", 
+    bio : "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste, autem dolorum accusantium ducimus architecto distinctio vero. Odio eaque unde, vero perspiciatis illum voluptatibus sunt quod excepturi! Illo natus sed magni!",
+    cost: "40",
+    whatsapp: "33988928922",
+    weekday: [1,3,4,5,6], 
+    time_from: [720, 720, 720, 720, 720], 
+    time_to: [810, 810, 810, 810, 810]
+  },
+];
+
+const hasProffys = proffys.length > 0;
+
 const express = require("express");
 const server = express();
 const port = 3000;
 
 const nunjucks = require("nunjucks");
+
 nunjucks.configure("src/pages", {
   express: server,
   noCache: true,
@@ -16,7 +44,7 @@ server.get("/", (req, res) => {
 });
 
 server.get("/study", (req, res) => {
-  return res.render("page-study.html");
+  return res.render("page-study.html", { hasProffys, proffys });
 });
 
 server.get("/give-classes", (req, res) => {
