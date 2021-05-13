a5e26883991a56318ed6d131606552ba714ffa5e
ramonis567 <ramonmatosferreira@gmail.com>
2021-05-13 17:37:41 -0300

    give-classes form data to database

--- src/pages.js
+++ src/pages.js
@@ -4,9 +4,9 @@ const Database = require("./database/db.js");
 async function renderPageStudy(req, res){
   const filters = req.query;
 
-  if((!filters.subject)||(!filters.weekday)||(!filters.hours)){
-    console.log("Campos vazios");
-    return res.render("page-study.html", { filters, subjects, weekdays });
+  if((!filters.subject) || (!filters.weekday) || (!filters.hours)){
+    console.log("Campos vazios", filters.subject, filters.weekday, filters.hours);
+    return res.render("study.html", { filters, subjects, weekdays });
   } 
     
   filters.hours = transformHoursToMinutes(filters.hours);
@@ -36,12 +36,12 @@ async function renderPageStudy(req, res){
 
     if(proffys.length = 0){
       console.log("Nenhum professor");
-      return res.render("page-study.html", { filters, subjects, weekdays });
+      return res.render("study.html", { filters, subjects, weekdays });
     }
     else {
       console.log("Há professores");
       console.log(proffys);
-      return res.render("page-study.html", { proffys, filters, subjects, weekdays });
+      return res.render("study.html", { proffys, filters, subjects, weekdays });
     } 
   } catch (error) {
     console.log(error);
@@ -49,20 +49,34 @@ async function renderPageStudy(req, res){
 }
 
 function renderGiveClasses(req, res){
-  const data = req.query;
-  const hasData = Object.keys(data).length > 0;
-
-  if(hasData){
-    data.subject = getSubject(data.subject);
-    proffys.push(data);
-    return res.redirect("/study");
-  }
-
   return res.render("give-classes.html", { subjects, weekdays });
 }
 
-async function renderSaveClasses (req, res) {
-  console.log("teste");
+function renderSaveClasses (req, res) {
+  const createProffy = require("./database/createProffy.js");
+
+  const proffyValue = {
+    name: req.body.name,
+    avatar: req.body.avatar,
+    whatsapp: req.body.whatsapp,
+    bio : req.body.bio
+  };
+
+  const classValue = {
+    subject: req.body.subject,
+    cost: req.body.cost
+    // proffy_id => function createProffy
+  };
+
+  const classScheduleValues = req.body.weekday.map((weekday, index) => {
+      return {
+        // class_id => function createProffy
+        weekday: weekday, 
+        time_from: transformHoursToMinutes(req.body.time_from[index]), 
+        time_to: transformHoursToMinutes(req.body.time_to[index]),
+      }  
+    } 
+  )
 }
 
 module.exports = {
